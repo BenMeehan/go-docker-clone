8017abb596f28b90c171bbded02d0a56efa626a6
Ben Meehan <benmeehan111@gmail.com>
2024-04-27 13:20:20 +0530

    pass stage

--- app/main.go
+++ app/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
+	fmt.Println("Logs from your program will appear here!")
+
 	command := os.Args[3]
-	args := os.Args[4:len(os.Args)]
+	args := os.Args[4:]
 
 	cmd := exec.Command(command, args...)
 
@@ -19,7 +22,14 @@ func main() {
 
 	err := cmd.Run()
 	if err != nil {
-		fmt.Printf("Err: %v", err)
+		// Check if the error is an exit error
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			// Get the exit code from the exit error
+			exitCode := exitErr.Sys().(syscall.WaitStatus).ExitStatus()
+			// Exit with the same exit code
+			os.Exit(exitCode)
+		}
+		// If it's not an exit error, exit with code 1
 		os.Exit(1)
 	}
 }
